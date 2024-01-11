b29070685b3c0f0f16893a13296eadc1c952d06c
Tyler Smith <tylerlwsmith@gmail.com>
2024-01-10 21:40:51 -0800

    Add JSON struct tags

--- webapp/wp/types.go
+++ webapp/wp/types.go
@@ -6,39 +6,39 @@ import (
 )
 
 type WPPost struct {
-	Id            int
-	Date          time.Time
-	DateGmt       time.Time
-	Guid          WPGuid
-	Modified      time.Time
-	ModifiedGmt   time.Time
-	Slug          string
-	Type          string
-	Link          url.URL
-	Title         WPRenderable
-	Content       WPRenderable
-	Excerpt       WPRenderable
-	Author        int
-	FeaturedMedia int
-	CommentStatus string
-	PingStatus    string
-	Sticky        bool
-	Template      string
-	Format        string
-	Meta          WPMeta
-	Categories    []int
-	Tags          []int
+	Id            int          `json:"id"`
+	Date          time.Time    `json:"date"`
+	DateGmt       time.Time    `json:"date_gmt"`
+	Guid          WPGuid       `json:"guid"`
+	Modified      time.Time    `json:"modified"`
+	ModifiedGmt   time.Time    `json:"modified_gmt"`
+	Slug          string       `json:"slug"`
+	Type          string       `json:"type"`
+	Link          url.URL      `json:"url"`
+	Title         WPRenderable `json:"title"`
+	Content       WPRenderable `json:"content"`
+	Excerpt       WPRenderable `json:"excerpt"`
+	Author        int          `json:"author"`
+	FeaturedMedia int          `json:"featured_media"`
+	CommentStatus string       `json:"comment_status"`
+	PingStatus    string       `json:"ping_status"`
+	Sticky        bool         `json:"sticky"`
+	Template      string       `json:"template"`
+	Format        string       `json:"format"`
+	Meta          WPMeta       `json:"meta"`
+	Categories    []int        `json:"categories"`
+	Tags          []int        `json:"tags"`
 }
 
 type WPGuid struct {
-	Rendered url.URL
+	Rendered url.URL `json:"rendered"`
 }
 
 type WPRenderable struct {
-	Rendered  string
-	Protected bool
+	Rendered  string `json:"rendered"`
+	Protected bool   `json:"protected,omitempty"`
 }
 
 type WPMeta struct {
-	Footnotes string
+	Footnotes string `json:"footnotes"`
 }
