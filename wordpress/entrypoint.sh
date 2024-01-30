deab6acbb51e125e6f4ecea6795bc27024c46da9
Tyler Smith <tylerlwsmith@gmail.com>
2024-01-29 20:17:52 -0800

    Cleanup entrypoint.sh

--- wordpress/entrypoint.sh
+++ wordpress/entrypoint.sh
@@ -1,21 +1,18 @@
 #! /usr/bin/env bash
 set -e
 
-# TODO: Replace with wait script.
 wait-for-it ${DB_HOST:-database}:3306 -- echo "connected to database"
 
-# if ! wp core is-installed  > /dev/null; then
-#     # WP is not installed. Let's try installing it.
-#     wp core install \
-#         --url=$WORDPRESS_URL \
-#         --title=WordPress \
-#         --admin_user=$WORDPRESS_USERNAME \
-#         --admin_password=$WORDPRESS_PASSWORD \
-#         --admin_email=$WORDPRESS_EMAIL \
-#         --skip-email
-# fi
+if ! wp core is-installed > /dev/null; then
+    # TODO: setup WordPress site if it does not exist. Example:
 
-# wp post create --post_type=post --post_title="$(date)" --post_status="publish"
-# wp theme list
+    # wp core install \
+    #     --url=$WORDPRESS_URL \
+    #     --title=WordPress \
+    #     --admin_user=$WORDPRESS_USERNAME \
+    #     --admin_password=$WORDPRESS_PASSWORD \
+    #     --admin_email=$WORDPRESS_EMAIL \
+    #     --skip-email
+fi
 
 exec "$@"
